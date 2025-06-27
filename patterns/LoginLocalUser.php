6eb021f31c870969a1d461bbcf16e9422ba56c22
jsmalc-algebra <jsmalc2@algebra.hr>
2025-06-27 18:31:52 +0200

    Moved the dependency on the User class to UserBuilder.php interface as all user builder classes will need to implement it and through it would have access to Users rather than having to implement it in each new UserBuilder blueprint

--- patterns/LoginLocalUser.php
+++ patterns/LoginLocalUser.php
@@ -1,6 +1,5 @@
 <?php
 require_once "UserBuilder.php";
-require_once "User.php";
 class LoginLocalUser implements UserBuilder
 {
     private User $user;
--- patterns/UserBuilder.php
+++ patterns/UserBuilder.php
@@ -1,5 +1,5 @@
 <?php
-
+    require_once '../User.php';
 interface UserBuilder
 {
     public function reset();
